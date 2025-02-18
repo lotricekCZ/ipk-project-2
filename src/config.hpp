517d37b13fbe56ece910c476a056c3773dacaf9b
Jakub Ramašeuski <jakub@skaryna.net>
2025-02-18 20:31:39 +0100

    feat: main and config added

---
+++ src/config.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <cstdint>
+#include <string>
+
+namespace config
+{
+	const std::string secret = "41d59e50-0f35-44c1-abbe-48134dbc317f";
+	std::string hostname = "anton5.fit.vutbr.cz";
+	std::uint16_t port = 4567;
+	std::string username = "xramas01@stud.fit.vut.cz";
+}
+
+#endif
