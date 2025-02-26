1568d2268810042333e46cea1d3b7a2d6d8b07e1
Jakub Ramašeuski <jakub@skaryna.net>
2025-02-26 18:47:14 +0100

    feat: config made as separate shared library

---
+++ src/config.cpp
@@ -0,0 +1,9 @@
+#include "config.hpp"
+
+namespace config
+{
+	const std::string secret = "41d59e50-0f35-44c1-abbe-48134dbc317f";
+	const std::string hostname = "anton5.fit.vutbr.cz";
+	std::uint16_t port = 4567;
+	std::string username = "xramas01@stud.fit.vut.cz";
+}
--- src/config.hpp
+++ src/config.hpp
@@ -6,10 +6,10 @@
 
 namespace config
 {
-	const std::string secret = "41d59e50-0f35-44c1-abbe-48134dbc317f";
-	std::string hostname = "anton5.fit.vutbr.cz";
-	std::uint16_t port = 4567;
-	std::string username = "xramas01@stud.fit.vut.cz";
+	extern const std::string secret;
+	extern const std::string hostname;
+	extern std::uint16_t port;
+	extern std::string username;
 }
 
 #endif
