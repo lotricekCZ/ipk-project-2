7f9626b8319cce2b04440d6758dc29d2eba5f63c
Jakub Ramašeuski <jakub@skaryna.net>
2025-03-03 23:16:22 +0100

    feat(config): add displayName

--- src/config.cpp
+++ src/config.cpp
@@ -4,6 +4,7 @@ namespace config
 {
 	const std::string secret = "41d59e50-0f35-44c1-abbe-48134dbc317f";
 	std::string hostname = "anton5.fit.vutbr.cz";
+	std::string displayName = "lotricek";
 	std::uint16_t port = 4567;
 	std::uint16_t timeout = 250;
 	std::uint8_t retransmissions = 3;
--- src/config.hpp
+++ src/config.hpp
@@ -10,6 +10,7 @@ namespace config
 	extern std::string hostname;
 	extern std::uint16_t port;
 	extern std::string username;
+	extern std::string displayName;
 	extern std::uint16_t timeout;
 	extern std::uint8_t retransmissions;
 }
