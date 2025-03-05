90a6ac49ff384c45fa99163e40954f82c55a7475
Jakub Ramašeuski <jakub@skaryna.net>
2025-03-05 17:38:30 +0100

    fix(IOHandler): command regex and AUTH args swap  - AUTH args were swapped, this applies no longer  - command regex was able to ignore /help because there was no space after it

--- src/io_handler/io_handler.cpp
+++ src/io_handler/io_handler.cpp
@@ -25,7 +25,7 @@ void IOHandler::printMessage(formats::Message &message)
 formats::Message IOHandler::readMessage(std::string &data)
 {
 	formats::Message message;
-	std::regex r("^\\/(join|auth|rename|help)\\s(.*)");
+	std::regex r("^\\/(join|auth|rename|help)\\s*(.*)");
 	std::smatch m;
 	std::string helpMessage = "Available commands:\n\t/auth USERNAME SECRET DISPLAYNAME\tSends AUTH message with the data provided from the command to the server (and correctly handles the Reply message), locally sets the DisplayName value (same as the /rename command)\n\t/join CHANNEL_ID\t\t\t\t\tSends JOIN message with channel name from the command to the server (and correctly handles the Reply message)\n\t/rename DISPLAYNAME\t\t\t\tLocally changes the display name of the user to be sent with new messages/selected commands\n\t/help\t\t\t\t\t\t\t\tDisplay this help message and exit.";
 	auto count = [](std::smatch m) -> int
@@ -64,8 +64,8 @@ formats::Message IOHandler::readMessage(std::string &data)
 				if (count(m2) != 3)
 					throw std::runtime_error("Not enough parameters given!");
 				config::username = m2[1].str();
-				config::secret = m2[2].str();
-				config::displayName = m2[3].str();
+				config::displayName = m2[2].str();
+				config::secret = m2[3].str();
 				message.setAuthor(config::displayName);
 				message.setType(formats::AUTH);
 				return message;
