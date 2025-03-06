07904f319dc347e15de9a7d9d35874ede49a25d2
Jakub Ramašeuski <jakub@skaryna.net>
2025-03-06 12:17:43 +0100

    feat(fsm): Edge/Node func impl moved to header  - function implementations were moved to header due to compilation limitations of template classes

--- src/fsm/edge.hpp
+++ src/fsm/edge.hpp
@@ -20,8 +20,36 @@ public:
 	template <typename... Args>
 	Edge(std::shared_ptr<EdgeNode>, Args &&...);
 	bool accept(E &input);
-	std::function<void(E &)> perform;
+	// std::function<void(E &)> perform;
 	void setTarget(std::shared_ptr<EdgeNode> target);
 	std::shared_ptr<EdgeNode> getTarget();
 };
+
+template <typename T, typename E>
+template <typename ... Args>
+Edge<T, E>::Edge(std::shared_ptr<EdgeNode> target, Args &&...args){
+	this->target = target;
+	this->functions = {std::forward<Args>(args)...};
+	// this->perform = [](T &){};
+}
+
+template <typename T, typename E>
+bool Edge<T, E>::accept(E &input){
+	if(functions.empty()) return true;
+	for(auto f : functions){
+		if(f(input)) return true;
+	}
+	return false;
+}
+
+template <typename T, typename E>
+void Edge<T, E>::setTarget(std::shared_ptr<EdgeNode> target){
+	this->target = target;
+}
+
+template <typename T, typename E>
+std::shared_ptr<Node<T, E>> Edge<T, E>::getTarget(){
+	return this->target;
+}
+
 #endif
--- src/fsm/node.hpp
+++ src/fsm/node.hpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <memory>
-// #include "edge.hpp"
 
 #ifndef NODE_HPP
 #define NODE_HPP
@@ -21,5 +20,28 @@ public:
 	void addEdge(std::shared_ptr<NodeEdge> edge);
 	std::shared_ptr<Node<T, E>> next(E input);
 };
+
+
+template <typename T, typename E>
+template <typename... Args>
+void Node<T, E>::assignEdges(Args &&...args)
+{
+	this->edges = {std::forward<Args>(args)...};
+}
+
+template <typename T, typename E>
+void Node<T, E>::addEdge(std::shared_ptr<NodeEdge> edge){
+	this->edges.push_back(edge);
+}
+
+template <typename T, typename E>
+std::shared_ptr<Node<T, E>> Node<T, E>::next(E input){
+	for(auto edge : edges){
+		if(edge->accept(input)){
+			return edge->getTarget();
+		}
+	}
+	return nullptr;
+}
 
 #endif
